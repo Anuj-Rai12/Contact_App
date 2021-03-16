a225a774870f3af36cd439be01cbdb7e4fe139b9
Anuj <anujrai04082002.0@gmail.com>
2021-03-16 15:29:55 +0530

    Further modifecation done on the Getting image Form gallery and Camera status:Successfully

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -4,8 +4,7 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
@@ -14,7 +13,6 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.roomdatabase.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -47,9 +45,9 @@ class MainActivity : AppCompatActivity() {
     fun requestPermission() {
         val collectAllPermission = mutableListOf<String>()
         if (!checkCameraPermission())
-            collectAllPermission.add("CAMERA")
+            collectAllPermission.add(Manifest.permission.CAMERA)
         if (!checkCameraPermission())
-            collectAllPermission.add("READ_EXTERNAL_STORAGE")
+            collectAllPermission.add(Manifest.permission.READ_EXTERNAL_STORAGE)
         if (collectAllPermission.isNotEmpty())
             ActivityCompat.requestPermissions(this, collectAllPermission.toTypedArray(), 101)
     }
@@ -62,9 +60,9 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 101 && grantResults.isNotEmpty()) {
             for (i in grantResults.indices) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED)
-                    Toast.makeText(this, "permsion is granted for $permissions", Toast.LENGTH_SHORT)
-                        .show()
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("PERMISSION", "The Permission for $permissions")
+                }
             }
         }
     }
--- app/src/main/java/com/example/roomdatabase/utils/BottomSheetFragment.kt
+++ app/src/main/java/com/example/roomdatabase/utils/BottomSheetFragment.kt
@@ -1,18 +1,27 @@
 package com.example.roomdatabase.utils
 
 import android.Manifest
+import android.R.attr
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.BitmapRegionDecoder
+import android.net.Uri
 import android.os.Bundle
-import android.view.ContextMenu
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.roomdatabase.R
 import com.example.roomdatabase.databinding.BottonLayoutBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.io.FileNotFoundException
+
 
 class BottomSheetFragment : BottomSheetDialogFragment() {
     lateinit var binding: BottonLayoutBinding
@@ -24,12 +33,57 @@ class BottomSheetFragment : BottomSheetDialogFragment() {
         //return super.onCreateView(inflater, container, savedInstanceState)
         binding = DataBindingUtil.inflate(inflater, R.layout.botton_layout, container, false)
         binding.btnopencam.setOnClickListener {
-            Toast.makeText(activity, "Hello fucker", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(activity, "Hello fucker", Toast.LENGTH_SHORT).show()
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, 111)
+
         }
         binding.btnopengal.setOnClickListener {
-            Toast.makeText(activity, "Hello fucker form gallery", Toast.LENGTH_SHORT).show()
+            val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, 404)
         }
         return binding.root
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            404 -> {
+                if (resultCode == RESULT_OK) {
+                    try {
+                        val inputStream =requireActivity().contentResolver.openInputStream(data?.data!!)
+                        val bitmapImage = BitmapFactory.decodeStream(inputStream)
+                        Toast.makeText(
+                            activity,
+                            "Gallery is working file ${bitmapImage}",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        /*Further modified status:Failed*/
+                    } catch (e: FileNotFoundException) {
+                        Toast.makeText(activity, "File not found", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            111 -> {
+                if (resultCode == RESULT_OK) {
+                    val bitmap = data?.getParcelableExtra<Bitmap>("data")
+                    Toast.makeText(
+                        activity,
+                        "Gallery is working file ${bitmap}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+        }
+    }
+
+    private fun checkCameraPermission() =
+        (context?.let {
+            ActivityCompat.checkSelfPermission(
+                it,
+                Manifest.permission.CAMERA
+            )
+        } == PackageManager.PERMISSION_GRANTED)
 }
