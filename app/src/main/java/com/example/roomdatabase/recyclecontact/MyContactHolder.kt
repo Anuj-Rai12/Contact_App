2c7d494243e82242ed68bfcd60832dcdcc5af17f
Anuj <anujrai04082002.0@gmail.com>
2021-03-19 08:39:50 +0530

    Adding RecycleView

---
+++ app/src/main/java/com/example/roomdatabase/recyclecontact/MyContactHolder.kt
@@ -0,0 +1,7 @@
+package com.example.roomdatabase.recyclecontact
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MyContactHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+}
--- app/src/main/java/com/example/roomdatabase/recyclecontact/MyContactRecycle.kt
+++ app/src/main/java/com/example/roomdatabase/recyclecontact/MyContactRecycle.kt
@@ -1,4 +1,20 @@
 package com.example.roomdatabase.recyclecontact
 
-class MyContactRecycle(private val U) {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdatabase.mycontactdb.MyContact
+
+class MyContactRecycle() : RecyclerView.Adapter<MyContactHolder>() {
+    private lateinit var myAllMyContact: ArrayList<MyContact>
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyContactHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MyContactHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
 }
